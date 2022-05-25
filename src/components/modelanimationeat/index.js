c7bb95a1b40259d325a85c747eeb1b74946b7958
joseproig <josep.roig.torres@salle.url.edu>
2022-05-25 12:15:54 +0200

    feat: Compress eat gif

--- src/components/modelanimationeat/index.js
+++ src/components/modelanimationeat/index.js
@@ -2,7 +2,7 @@ function ModelAnimationEat(props) {
     
   //const { actions } = useAnimations(animations, group)
   return (
-    <img src={require('./../../assets/3DModels/eat.gif')} width="840" height="400" alt="loading..." />
+    <img src={require('./../../assets/3DModels/eat_2.gif')} width="840" height="400" alt="loading..." />
   );
 }
 export default ModelAnimationEat;
