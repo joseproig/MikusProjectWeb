acacdc82a227c691fbe61c7072e5e3da0840959c
joseproig <josep.roig.torres@salle.url.edu>
2022-05-25 12:31:51 +0200

    feat: Added compress animation of shower

--- src/components/modelanimationshower/index.js
+++ src/components/modelanimationshower/index.js
@@ -2,7 +2,7 @@ function ModelAnimationShower(props) {
     
     //const { actions } = useAnimations(animations, group)
     return (
-      <img src={require('./../../assets/3DModels/shower.gif')} width="840" height="400" alt="loading..." />
+      <img src={require('./../../assets/3DModels/shower_2.gif')} width="840" height="400" alt="loading..." />
     );
 }
 export default ModelAnimationShower;
